0ad822df7710029dee1c10f6e5005b9c3a4cfc6c
wangxuan <2324717228@qq.com>
2016-03-10 19:05:54 +0800

    complete Impement

--- src/ImplementStrStr.java
+++ src/ImplementStrStr.java
@@ -1,24 +1,19 @@
-/**
- * Created by sdww on 16-3-8.
- * Implement strStr().
- *
- * Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
- *
- * Subscribe to see which companies asked this question
- *
- */
-//KMP算法
-public class ImplementStrStr {
-  public int strStr(String haystack, String needle) {
+public class Solution {
+    public int strStr(String haystack, String needle) {
+    if(needle.length() == 0) {
+        return 0;
+    }
     //计算needle的左移数组
     int[] array = new int[needle.length()];
-    array[0] = 0;
+    array[0] = 1;
     for(int i = 1; i < needle.length(); i++) {
-      for(int startIndex = 1; startIndex <= i; startIndex++) {
-        if(strComp(needle, needle.substring(startIndex, i - 1))) {
+      for(int startIndex = array[i-1]; startIndex < i; startIndex++) {
+        if(strComp(needle, startIndex, i - 1)) {
           array[i] = startIndex;
+          break;
         }
       }
+      if(array[i] == 0) array[i] = i;
     }
     //具体算法
     int hIndex = 0;
@@ -31,7 +26,7 @@ public class ImplementStrStr {
         nIndex++;
       }
       else {
-        if(array[nIndex] == 0) {
+        if(nIndex == 0) {
           hIndex++;
         } else {
           nIndex -= array[nIndex];
@@ -40,10 +35,9 @@ public class ImplementStrStr {
     }
   }
 
-  private boolean strComp(String str1, String str2) {
-    if(str1.length() < str2.length()) return false;
-    for(int i = 0; i < str2.length(); i++) {
-      if(str1.charAt(i) != str2.charAt(i)) {
+  private boolean strComp(String str, int startIndex, int endIndex) {
+    for(int i = 0; i <= endIndex - startIndex; i++) {
+      if(str.charAt(i) != str.charAt(i + startIndex)) {
         return false;
       }
     }
